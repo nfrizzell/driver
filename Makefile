ecd62cdf3ce81d6bac178eaedcc19b59ad4b0bb2
nfrizzell <54083373+nfrizzell@users.noreply.github.com>
2021-12-09 19:59:56 -0500

    more features

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
-obj-m += hello_world.o
+ifneq ($(KERNELRELEASE),)
+	obj-m += hello_world.o
+
+else
+	KERNELDIR ?= /lib/modules/$(shell uname -r)/build
+	PWD  := $(shell pwd)
 
 default:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	make -C $(KERNELDIR) M=$(PWD) modules
 
 clean:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	make -C $(KERNELDIR) M=$(PWD) clean
 
 install: hello_world.ko
 	install -d /lib/modules/$(shell uname -r)/kernel/misc/
 	install -m 644 hello_world.ko /lib/modules/$(shell uname -r)/kernel/misc/
 	depmod
+endif
--- hello_world.c
+++ hello_world.c
@@ -1,18 +1,29 @@
-#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/moduleparam.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
+static char * who = "world";
+static int repeat = 1;
 
-static int hello_init(void)
+module_param(who, charp, S_IRUGO);
+module_param(repeat, int, S_IRUGO);
+
+static int __init hello_init(void)
 {
-    printk(KERN_WARNING "Hello, world\n");
-    return 0;
+	int i;
+	for (i = 0; i < repeat; i++)
+		printk(KERN_ALERT "hello %s\n", who);
+
+	return 0;
 }
 
-static void hello_exit(void)
+static void __exit hello_exit(void)
 {
-    printk(KERN_WARNING "Goodbye, cruel world\n");
+	printk(KERN_ALERT "'hello_world' module unloaded\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
